25f8155bd6107b97a9201901cfcfbb20c4be334c
Ernest B.M 243 <emudjambere@gmail.com>
2020-04-20 16:22:52 -0400

    Add files via upload

---
+++ Test_2/plctest2b.cpp
@@ -0,0 +1,27 @@
+/*
+ * plctest2b.cpp
+ *
+ *  Created on: Apr 20, 2020
+ *      Author: $Ernest_243
+ */
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int CRICKET = 1;
+    int TENNIS = 13;
+    int FOOTBALL = 26;
+    int BASKETBALL = 3;
+
+
+    int play = 26;
+    int sunday = 0 , monday = 0 , tu = 3 , wed = 4 , thu = 5 , fri = 6, sat = 7;
+    int w = 0;
+    if(w==sunday && play ==FOOTBALL){
+        cout<<"Its Sunday and we have FootBall Match"<<endl;
+     }
+    return 0;
+}
