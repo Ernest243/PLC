9089c39db9566fb70da10be9098a81d59d0bb3ff
Ernest B.M 243 <emudjambere@gmail.com>
2020-04-29 22:14:10 -0400

    Add files via upload

---
+++ final_q5_plc.java
@@ -0,0 +1,17 @@
+public class Test
+{
+    // Variables
+    public int i, n, j, x;
+
+    // Labeled loop
+    reject: for(i = 1; i <= n; i++)
+    {
+        for(j = 1; j = n; j++)
+        {
+            if(x[i][j] != 0)
+                break;
+            System.out.println("First all-zero row is: " + i);
+            break reject;
+        }
+    }
+}
